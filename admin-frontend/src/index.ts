02e878c64d86bb7b818c0a79a6a3433b110962b1
Ryan Wang <i@ryanc.cc>
2022-08-29 11:22:49 +0800

    Initial commit

---
+++ admin-frontend/src/index.ts
@@ -0,0 +1,40 @@
+import { definePlugin, BasicLayout } from "@halo-dev/admin-shared";
+import DefaultView from "./views/DefaultView.vue";
+import { IconGrid } from "@halo-dev/components";
+import "./styles/index.css";
+
+export default definePlugin({
+  name: "PluginTemplate",
+  components: [],
+  routes: [
+    {
+      path: "/hello-world",
+      component: BasicLayout,
+      children: [
+        {
+          path: "",
+          name: "HelloWorld",
+          component: DefaultView,
+          meta: {
+            permissions: ["plugin:apples:view"],
+          },
+        },
+      ],
+    },
+  ],
+  menus: [
+    {
+      name: "From PluginTemplate",
+      items: [
+        {
+          name: "HelloWorld",
+          path: "/hello-world",
+          icon: IconGrid,
+        },
+      ],
+    },
+  ],
+  extensionPoints: {},
+  activated() {},
+  deactivated() {},
+});
