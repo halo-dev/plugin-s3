2b0dd98575dbc829da0b72a0879ed4b3430fb26b
Ryan Wang <i@ryanc.cc>
2024-02-03 23:50:55 +0800

    refactor: place the entry for the S3 link page in the side menu (#116)
    将 S3 关联页面放置在左侧菜单。
    
    /kind improvement
    
    Note: 此特性需要运行在 Halo 2.12 才生效，所以此 PR 同时修改了 requires。
    
    <img width="1920" alt="image" src="https://github.com/halo-dev/plugin-s3/assets/21301288/a78d7837-81c2-4c50-ad19-2c40e74cbc41">
    
    <img width="1920" alt="image" src="https://github.com/halo-dev/plugin-s3/assets/21301288/41edeed8-9ce0-40d1-82a9-1fb376a08c51">
    
    
    ```release-note
    将 S3 关联页面放置在左侧菜单。
    ```

--- console/src/index.ts
+++ console/src/index.ts
@@ -1,27 +1,35 @@
-import type {Attachment} from "@halo-dev/api-client";
-import type {PluginTab} from "@halo-dev/console-shared";
-import {definePlugin} from "@halo-dev/console-shared";
+import type { Attachment } from "@halo-dev/api-client";
+import { definePlugin } from "@halo-dev/console-shared";
 import S3Link from "./views/S3Link.vue";
-import S3Unlink from "./views/S3Unlink.vue"
-import type {Ref} from "vue";
-import {markRaw} from "vue";
+import S3Unlink from "./views/S3Unlink.vue";
+import type { Ref } from "vue";
+import { markRaw } from "vue";
+import CarbonFolderDetailsReference from "~icons/carbon/folder-details-reference";
 
 export default definePlugin({
   components: {},
-  routes: [],
-  extensionPoints: {
-    "plugin:self:tabs:create": (): PluginTab[] => {
-      return [
-        {
-          id: "s3-link",
-          label: "关联S3文件",
-          // @ts-ignore
-          component: markRaw(S3Link),
-          permissions: ["plugin:s3os:link"]
+  routes: [
+    {
+      parentName: "ToolsRoot",
+      route: {
+        path: "s3-link",
+        name: "S3Link",
+        component: S3Link,
+        meta: {
+          title: "S3 关联",
+          description: "提供将 S3 存储桶中的文件关联到 Halo 中的功能。",
+          searchable: true,
+          permissions: ["plugin:s3os:link"],
+          menu: {
+            name: "S3 关联",
+            icon: markRaw(CarbonFolderDetailsReference),
+            priority: 0,
+          },
         },
-      ];
+      },
     },
-    // @ts-ignore
+  ],
+  extensionPoints: {
     "attachment:list-item:operation:create": (attachment: Ref<Attachment>) => {
       return [
         {
@@ -29,10 +37,14 @@ export default definePlugin({
           component: markRaw(S3Unlink),
           permissions: ["plugin:s3os:unlink"],
           props: {
-            "attachment": attachment,
+            attachment: attachment,
           },
-          hidden: !(attachment.value.metadata.annotations
-            && attachment.value.metadata.annotations["s3os.plugin.halo.run/object-key"])
+          hidden: !(
+            attachment.value.metadata.annotations &&
+            attachment.value.metadata.annotations[
+              "s3os.plugin.halo.run/object-key"
+            ]
+          ),
         },
       ];
     },
