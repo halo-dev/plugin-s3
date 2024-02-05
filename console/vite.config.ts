11cbfdb9bb9a2c69de6eb22b46c7acabd0f3c90b
Ryan Wang <i@ryanc.cc>
2024-02-05 10:56:31 +0800

    chore: simplify bundler config (#117)
    使用 `@halo-dev/ui-plugin-bundler-kit` 简化构建配置。
    
    /kind improvement
    
    ```release-note
    None
    ```

--- console/vite.config.ts
+++ console/vite.config.ts
@@ -4,44 +4,18 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import VueJsx from "@vitejs/plugin-vue-jsx";
 import Icons from "unplugin-icons/vite";
+import { HaloUIPluginBundlerKit } from "@halo-dev/ui-plugin-bundler-kit";
 
-const pluginEntryName = "PluginS3ObjectStorage";
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Vue(), VueJsx(), Icons({ compiler: "vue3" })],
+  plugins: [
+    Vue(),
+    VueJsx(),
+    Icons({ compiler: "vue3" }),
+    HaloUIPluginBundlerKit(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  build: {
-    outDir: fileURLToPath(
-      new URL("../src/main/resources/console", import.meta.url)
-    ),
-    emptyOutDir: true,
-    lib: {
-      entry: "src/index.ts",
-      name: pluginEntryName,
-      formats: ["iife"],
-      fileName: () => "main.js",
-    },
-    rollupOptions: {
-      external: [
-        "vue",
-        "@halo-dev/console-shared",
-        "@halo-dev/components",
-        "vue-router",
-      ],
-      output: {
-        globals: {
-          vue: "Vue",
-          "vue-router": "VueRouter",
-          "@halo-dev/components": "HaloComponents",
-          "@halo-dev/console-shared": "HaloConsoleShared",
-        },
-        extend: true,
-      },
     },
   },
 });
